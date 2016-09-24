1610cedb82f1a788482135fc42ce5dadbc390e3d
nitz <nitz.raz@gmail.com>
2016-09-24 20:43:18 +0300

    Static-ed everything that I can

--- dns-override.c
+++ dns-override.c
@@ -5,26 +5,26 @@
 #include <string.h>
 #include <stdlib.h>
 
-const char *RESOLV_CONF = "/etc/resolv.conf";
-const int RESOLV_SIZE = 16;
+static const char *RESOLV_CONF = "/etc/resolv.conf";
+static const int RESOLV_SIZE = 16;
 
-const char *RESOLV_ENV = "RESOLV_CONF";
+static const char *RESOLV_ENV = "RESOLV_CONF";
 
-FILE *(*original_fopen)(const char*, const char*) = NULL; 
-FILE *(*original_fopen64)(const char*, const char*) = NULL;
+static FILE *(*original_fopen)(const char*, const char*) = NULL; 
+static FILE *(*original_fopen64)(const char*, const char*) = NULL;
 
-void init_fopen_override() {
+static void init_fopen_override() {
   if (original_fopen == NULL)
     original_fopen = dlsym(RTLD_NEXT, "fopen");
   if (original_fopen64 == NULL)
     original_fopen64 = dlsym(RTLD_NEXT, "fopen64");
 }
 
-char *better_resolv = NULL;
-int better_resolv_inited=0;
+static char *better_resolv = NULL;
+static int better_resolv_inited=0;
 
 // No fmemopen64 so implementing the same for both functions
-FILE *init_better_resolv() {
+static FILE *init_better_resolv() {
   // Singleton
   if (!better_resolv_inited) {
     better_resolv_inited=1;
